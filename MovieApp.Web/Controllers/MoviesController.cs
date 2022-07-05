6ab6a0d96c7d6205434e617f10f067f1b7940712
Malatya <Malatya@DESKTOP-IJRSBPQ>
2022-07-05 15:21:11 +0300

    Proje dosyası ekle.

---
+++ MovieApp.Web/Controllers/MoviesController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MovieApp.Web.Data;
+using MovieApp.Web.Entity;
+using MovieApp.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApp.Web.Controllers
+{
+    //controller
+    public class MoviesController : Controller
+    {
+        private readonly MovieContext _context;
+            public MoviesController(MovieContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //localhost:5000/movies/list
+        //localhost:5000/movies/list/1
+        [HttpGet]
+        public IActionResult List(int? id,string q)
+        {
+            //{ontroller}/{action}/{id?}
+            //movies/list/2
+
+            //var controller = RouteData.Values["controller"];
+            //var action = RouteData.Values["action"];
+            //var genreid = RouteData.Values["id"];
+
+            //var kelime = HttpContext.Request.Query["q"].ToString();
+
+            //var movies = MovieRepository.Movies;
+            var movies = _context.Movies.AsQueryable();
+
+            if (id != null)
+            {
+                movies = movies
+                    .Include(m=>m.Genres)
+                    .Where(m => m.Genres.Any(g=>g.GenreId==id));
+            }
+            if(!string.IsNullOrEmpty(q))
+            {
+                movies = movies.Where(i =>
+                i.Title.ToLower().Contains(q.ToLower()) ||
+                i.Description.ToLower().Contains(q.ToLower()));
+            }
+
+            var model = new MoviesViewModel()      //---Görüntü Oluşturmak İçin Olan Paket---
+            {
+                Movies = movies.ToList()
+            };
+
+            return View("Movies",model);
+        }
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            return View(_context.Movies.Find(id));
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.Genres = new SelectList(_context.Genres.ToList(), "GenreId", "Name");
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Movie m)
+        {
+            if (ModelState.IsValid)
+            {
+                //MovieRepository.Add(m);
+                _context.Movies.Add(m);
+                _context.SaveChanges();
+                TempData["Message"] = $"{m.Title} isimli film eklendi.";
+                return RedirectToAction("List");
+            }
+            ViewBag.Genres = new SelectList(_context.Genres.ToList(), "GenreId", "Name");
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Genres = new SelectList(_context.Genres.ToList(), "GenreId", "Name");
+            return View(_context.Movies.Find(id));
+        }
+        [HttpPost]
+        public IActionResult Edit(Movie m)
+        {
+            if (ModelState.IsValid)
+            {
+                //MovieRepository.Edit(m);
+                _context.Movies.Update(m);
+                _context.SaveChanges();
+                return RedirectToAction("Details", "Movies", new { @id = m.MovieId });
+            }
+            ViewBag.Genres = new SelectList(_context.Genres.ToList(), "GenreId", "Name");
+            return View(m);
+        }
+        [HttpPost]
+        public IActionResult Delete(int MovieId,string Title)
+        {
+            //MovieRepository.Delete(MovieId);
+            var entity = _context.Movies.Find(MovieId);
+            _context.Movies.Remove(entity);
+            _context.SaveChanges();
+            TempData["Message"] = $"{Title} isimli film silindi.";
+            return RedirectToAction("List");
+        }
+
+    }
+}
---
+++ MovieApp.Web/Entity/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApp.Web.Entity
+{
+    public class Movie
+    {
+
+        //Primary Key => Id <Type Name> Id
+
+        //[Key,DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int MovieId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [MaxLength(500)]
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        [Required]
+        public List<Genre> Genres { get; set; }
+    }
+}
